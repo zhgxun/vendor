e0316a5bdfd9c9c30a5cdfba5b789220c25e2186
zhgxun <zhgxun1989@163.com>
2017-03-24 21:43:57 +0800

    update all

--- yiisoft/extensions.php
+++ yiisoft/extensions.php
@@ -51,7 +51,7 @@ return array (
   'yiisoft/yii2-debug' => 
   array (
     'name' => 'yiisoft/yii2-debug',
-    'version' => '2.0.7.0',
+    'version' => '2.0.9.0',
     'alias' => 
     array (
       '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
